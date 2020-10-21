4775b66b8e83328774c108e4b91dc150bd50401b
Giacomo Cregut <giacomo@cregut.it>
2020-10-21 21:58:45 +0200

    Updated readme with images

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # cpainterhelper
-a small script that converts adobe illustrator paths to dart for use in flutter
+A simple script that lets you convert adobe illustrator paths to code compatible with flutter's [CustomPainter](https://api.flutter.dev/flutter/rendering/CustomPainter-class.html)
+# How to use it
+First set up an illustrator document.
+It is very important that you keep in mind the aspect ratio since the script will make every measure proportional.
+![Creation Dialog](/images/doccreate.png) 
+
+Then draw your desired shape with the pen tool. At the moment only strokes / filled paths are supported (no fancy clipping paths or other advanced illustrator features). Feel free to contribute to the repository if you want.
+
+Here are some examples of admissible paths:
+![Example 1](/images/ex0.png) ![Example 2](/images/ex1.png) ![Example 3](/images/ex2.png) 
+
+Now, select File > Scripts > Other Script... or press ctrl + F12 on your keyboard. Navigate to the script and select it.
+![Script menu](/images/menu.png)
+
+The script will create a new text object containing the result.
+Each path will be converted to [cubicCurve]()s and will automatically be assigned  a [Paint]() for its fill and one for its stroke, if it has one.
+![Output](/images/output.png)
+
+The script iterates every path in the current active document. At the moment the order of output does not reflect the actual layers, so you'll have to adjust them yourself.
+
+The output should look something like this:
+
+```dart
+//Number 0
+Path path0 = Path(); 
+path0.moveTo(width * 0.12 , height * 0.48); 
+path0.cubicTo(width * 0.12, height * 0.48, width * 0.64, height * 0.52, width * 0.68, height * 0.52); 
+/*
+...
+*/
+canvas.drawPath(path0, spaint0); 
+
+//Number 1
+Path path1 = Path(); 
+path1.moveTo(width * 0.2 , height * 0.36); 
+path1.cubicTo(width * 0.25, height * 0.4, width * 0.45, height * 0.38, width * 0.48, height * 0.38); 
+/*
+...
+*/
+canvas.drawPath(path1, spaint1); 
+
+//Number 2
+Path path2 = Path(); 
+path2.moveTo(width * 0.17 , height * 0.25); 
+path2.cubicTo(width * 0.36, height * 0.26, width * 0.37, height * 0.26, width * 0.43, height * 0.22); 
+/*
+...
+*/
+canvas.drawPath(path2, spaint2); 
+```
+You can copy the result inside the [paint](https://api.flutter.dev/flutter/rendering/CustomPainter/paint.html) method of your CustomPainter. Add these to lines on top to make the code work:
+
+```dart
+final height = size.height;
+final width = size.width;
+  ```
+Here are the results from the previous examples rendered in flutter
+![Example 1](/images/ex0fltr.png) ![Example 2](/images/ex1fltr.png) ![Example 3](/images/ex2fltr.png) 
---
+++ src/cpainterhelper.js
@@ -0,0 +1,71 @@
+alert("Script started");
+var myDoc = app.activeDocument;
+var docWidth = myDoc.width;
+var docHeight = myDoc.height;
+var myPaths = myDoc.pathItems;
+var result = "";
+var xheight = "height * ";
+var xwidth = "width * ";
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
+
+function getPWidth(num) {
+    return roundToTwo(num / docWidth);
+}
+
+function getPHeight(num) {
+    return roundToTwo(-num / docHeight);
+}
+
+function addCubic(pathName, cp1, cp2, target) {
+    var res = ""
+    res += pathName + ".cubicTo(" + xwidth + getPWidth(cp1[0]) + ", " + xheight + getPHeight(cp1[1]) + ", " + xwidth + getPWidth(cp2[0]) + ", " + xheight + getPHeight(cp2[1]) + ", " + xwidth + getPWidth(target[0]) + ", " + xheight + getPHeight(target[1]) + "); \n";
+    return res;
+}
+
+for (var i = 0; i < myPaths.length; i++) {
+    var myPath = myPaths[i];
+    var isClosed = myPath.closed;
+    var isFilled = myPath.filled;
+    var isStroked = myPath.stroked
+    var myPoints = myPath.pathPoints;
+    var pathName = "path" + i;
+    var paintName = "paint" + i;
+    result += "//Number " + i + "\n";
+    result += "Path " + pathName + " = Path(); \n";
+    result += pathName + ".moveTo(" + xwidth + getPWidth(myPoints[0].anchor[0]) + " , " + xheight + getPHeight(myPoints[0].anchor[1]) + "); \n";
+    for (var j = 1; j < myPoints.length; j++) {
+        result += addCubic(pathName, myPoints[j - 1].rightDirection, myPoints[j].leftDirection, myPoints[j].anchor);
+    }
+    if (isClosed) {
+        result += addCubic(pathName, myPoints[myPoints.length - 1].rightDirection, myPoints[0].leftDirection, myPoints[0].anchor);
+    }
+    if (isFilled) {
+        var fPaintName = "f" + paintName;
+        var bgColor = new RGBColor();
+        bgColor = myPath.fillColor;
+        result += "Paint " + fPaintName + " = Paint(); \n";
+        result += fPaintName + ".color = Color.fromRGBO(" + bgColor.red + ", " + bgColor.green + ", " + bgColor.blue + ", 1.0); \n";
+        result += "canvas.drawPath(" + pathName + ", " + fPaintName + "); \n";
+    }
+    if (isStroked) {
+        var sPaintName = "s" + paintName;
+        var strokeColor = new RGBColor();
+        strokeColor = myPath.strokeColor;
+        var strokeCap = myPath.strokeCap;
+        var strokeJoin = myPath.strokeJoin;
+        var strokeWidth = myPath.strokeWidth;
+        var strokeMiterLimit = myPath.strokeMiterLimit;
+        result += "Paint " + sPaintName + " = Paint(); \n";
+        result += sPaintName + ".color = Color.fromRGBO(" + strokeColor.red + ", " + strokeColor.green + ", " + strokeColor.blue + ", 1.0); \n";
+        result += sPaintName + ".strokeWidth = " + strokeWidth + "; \n";
+        result += sPaintName + ".style = PaintingStyle.stroke; \n";
+        result += "canvas.drawPath(" + pathName + ", " + sPaintName + "); \n";
+    }
+    result += "\n";
+}
+var myText = myDoc.textFrames.pointText([0, 0]);
+myText.contents = result;
+alert(result);
