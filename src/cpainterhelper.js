12c065dc8272009be78d9e19a5f615d8973a57db
Giacomo Cregut <giacomo@cregut.it>
2020-10-23 21:53:50 +0200

    added support for radial gradients

--- src/cpainterhelper.js
+++ src/cpainterhelper.js
@@ -20,19 +20,33 @@ function getPHeight(num) {
 }
 
 function addCubic(pathName, cp1, cp2, target) {
-    var res = ""
+    var res = "";
     res += pathName + ".cubicTo(" + xwidth + getPWidth(cp1[0]) + ", " + xheight + getPHeight(cp1[1]) + ", " + xwidth + getPWidth(cp2[0]) + ", " + xheight + getPHeight(cp2[1]) + ", " + xwidth + getPWidth(target[0]) + ", " + xheight + getPHeight(target[1]) + "); \n";
     return res;
 }
 
+function addColor(color, opacity) {
+    var res = "";
+    if (color.typename == "RGBColor") {
+        res = "Color.fromRGBO(" + color.red + ", " + color.green + ", " + color.blue + ", " + opacity / 100 + ")";
+    } else if (color.typename == "GrayColor") {
+        var gray = 255 - (color.gray * 2.55);
+        res = "Color.fromRGBO(" + gray + ", " + gray + ", " + gray + ", " + opacity / 100 + ")";
+    } else {
+        alert("unrecognized color type!");
+    }
 
+    return res;
+
+}
+result += "Rect rect = Offset.zero & size; \n";
 for (var i = 0; i < myPaths.length; i++) {
     var invertedIndex = ((myPaths.length - 1) - i);
     var myPath = myPaths[invertedIndex];
     var isClosed = myPath.closed;
     var isFilled = myPath.filled;
     var isStroked = myPath.stroked
-    var opacity = myPath.opacity / 100;
+    var opacity = myPath.opacity;
     var myPoints = myPath.pathPoints;
     var pathName = "path" + i;
     var paintName = "paint" + i;
@@ -47,10 +61,45 @@ for (var i = 0; i < myPaths.length; i++) {
     }
     if (isFilled) {
         var fPaintName = "f" + paintName;
-        var bgColor = new RGBColor();
+        var bgColor;
         bgColor = myPath.fillColor;
         result += "Paint " + fPaintName + " = Paint(); \n";
-        result += fPaintName + ".color = Color.fromRGBO(" + bgColor.red + ", " + bgColor.green + ", " + bgColor.blue + ", " + opacity + "); \n";
+        if (bgColor.typename == "RGBColor") {
+            result += fPaintName + ".color = " + addColor(bgColor, opacity) + "; \n";
+        } else if (bgColor.typename == "GradientColor") {
+            var gradientName = "fgradient" + i;
+            if (bgColor.gradient.type == "GradientType.RADIAL") {
+                alert(bgColor.origin);
+                var gcolors = [];
+                var gstops = [];
+                for (var k = 0; k < bgColor.gradient.gradientStops.length; k++) {
+                    var myStop = bgColor.gradient.gradientStops[k];
+                    var newColor = addColor(myStop.color, myStop.opacity);
+                    gcolors.push(newColor);
+                    var newRampStop = "";
+                    newRampStop += myStop.rampPoint / 100;
+                    gstops.push(newRampStop);
+                }
+                result += "var " + gradientName + " = RadialGradient(\ncenter: const Alignment(";
+                result += (getPWidth(bgColor.origin[0]) * 2 - 1) + ", " + (getPHeight(bgColor.origin[1]) * 2 - 1) + "), \n";
+                result += "radius: " + bgColor.length / 1000 + ", \n";
+                alert(bgColor.length);
+                result += "colors: [\n";
+                for (var k = 0; k < gcolors.length; k++) {
+                    result += gcolors[k] + ", \n";
+                }
+                result += "], \n"
+                result += "stops: ["
+                for (var k = 0; k < gstops.length; k++) {
+                    result += gstops[k] + ", ";
+                }
+                result += "],\n);\n";
+                result += fPaintName + ".shader = " + gradientName + ".createShader(rect); \n";
+
+            }
+
+        }
+
 
         result += "canvas.drawPath(" + pathName + ", " + fPaintName + "); \n";
     }
@@ -63,7 +112,7 @@ for (var i = 0; i < myPaths.length; i++) {
         var strokeWidth = myPath.strokeWidth;
         var strokeMiterLimit = myPath.strokeMiterLimit;
         result += "Paint " + sPaintName + " = Paint(); \n";
-        result += sPaintName + ".color = Color.fromRGBO(" + strokeColor.red + ", " + strokeColor.green + ", " + strokeColor.blue + ", " + opacity + "); \n";
+        result += sPaintName + ".color = " + addColor(strokeColor, opacity) + "; \n";
         result += sPaintName + ".strokeWidth = " + strokeWidth + "; \n";
         if (strokeJoin != "StrokeJoin.MITERENDJOIN") {
             isRound = strokeJoin == "StrokeJoin.ROUNDENDJOIN";
