b53a1d8d288a760f4ce9e3bd698e545bf1cf1a3b
Giacomo Cregut <giacomo@cregut.it>
2020-10-24 13:53:41 +0200

    added lines and quadratic beziers

--- src/cpainterhelper.js
+++ src/cpainterhelper.js
@@ -19,10 +19,42 @@ function getPHeight(num) {
     return roundToFour(-num / docHeight);
 }
 
+function checkArrayEqual(arr1, arr2) {
+    return arr1[0] == arr2[0] && arr1[1] == arr2[1];
+}
+
 function addCubic(pathName, cp1, cp2, target) {
     var res = "";
     res += pathName + ".cubicTo(" + xwidth + getPWidth(cp1[0]) + ", " + xheight + getPHeight(cp1[1]) + ", " + xwidth + getPWidth(cp2[0]) + ", " + xheight + getPHeight(cp2[1]) + ", " + xwidth + getPWidth(target[0]) + ", " + xheight + getPHeight(target[1]) + "); \n";
     return res;
+}
+
+function addQuadratic(pathName, cp, target) {
+    var res = "";
+    res += pathName + ".quadraticBezierTo(" + xwidth + getPWidth(cp[0]) + ", " + xheight + getPHeight(cp[1]) + ", " + xwidth + getPWidth(target[0]) + ", " + xheight + getPHeight(target[1]) + "); \n";
+    return res;
+}
+
+function addLine(pathName, target) {
+    var res = "";
+    res += pathName + ".lineTo(" + xwidth + getPWidth(target[0]) + ", " + xheight + getPHeight(target[1]) + "); \n";
+    return res;
+}
+
+function addPathSegment(pathName, origin, target) {
+    var originAnchor = origin.anchor;
+    var cp1 = origin.rightDirection;
+    var cp2 = target.leftDirection;
+    var targetAnchor = target.anchor;
+    if (!checkArrayEqual(originAnchor, cp1) && !checkArrayEqual(targetAnchor, cp2)) {
+        return addCubic(pathName, cp1, cp2, targetAnchor);
+    } else if (checkArrayEqual(originAnchor, cp1) && checkArrayEqual(targetAnchor, cp2)) {
+        return addLine(pathName, targetAnchor);
+    } else if (checkArrayEqual(originAnchor, cp1)) {
+        return addQuadratic(pathName, cp2, targetAnchor);
+    } else {
+        return addQuadratic(pathName, cp1, targetAnchor);
+    }
 }
 
 function addColor(color, opacity) {
@@ -35,10 +67,9 @@ function addColor(color, opacity) {
     } else {
         alert("unrecognized color type!");
     }
-
     return res;
-
 }
+
 result += "Rect rect = Offset.zero & size; \n";
 for (var i = 0; i < myPaths.length; i++) {
     var invertedIndex = ((myPaths.length - 1) - i);
@@ -54,10 +85,10 @@ for (var i = 0; i < myPaths.length; i++) {
     result += "Path " + pathName + " = Path(); \n";
     result += pathName + ".moveTo(" + xwidth + getPWidth(myPoints[0].anchor[0]) + " , " + xheight + getPHeight(myPoints[0].anchor[1]) + "); \n";
     for (var j = 1; j < myPoints.length; j++) {
-        result += addCubic(pathName, myPoints[j - 1].rightDirection, myPoints[j].leftDirection, myPoints[j].anchor);
+        result += addPathSegment(pathName, myPoints[j - 1], myPoints[j]);
     }
     if (isClosed) {
-        result += addCubic(pathName, myPoints[myPoints.length - 1].rightDirection, myPoints[0].leftDirection, myPoints[0].anchor);
+        result += addPathSegment(pathName, myPoints[myPoints.length - 1], myPoints[0]);
     }
     if (isFilled) {
         var fPaintName = "f" + paintName;
